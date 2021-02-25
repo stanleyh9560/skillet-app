472c05b5a4a789069e9c82040d0d0724f5c621b7
Rou Wen <r3wen@ucsd.edu>
2021-02-25 15:30:14 -0800

    Additional popups for create and reviewCreate

--- routes/create.js
+++ routes/create.js
@@ -1,4 +1,3 @@
-
 /*
  * GET create recipe page.
  */
@@ -6,3 +5,22 @@
 exports.view = function(req, res){
   res.render('create');
 };
+/*
+var data = require("../data.json");
+
+exports.createRecipe = function(request, response) {
+	// Your code goes here
+  var name = request.body.recipeName;
+  var ingredients = request.body.ingredients;
+  var instructions = request.body.instructions;
+  // console.log("The name is: " + name + " and the description is: " + description);
+  var newRecipe = {
+    "name": name,
+    "ingredients": ingredients,
+    "instructions": instructions,
+    "imageURL": "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80"
+    }
+  data.recipes.push(newRecipe);
+  response.render('index', data);
+}
+*/
