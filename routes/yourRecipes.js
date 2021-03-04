f7b4223dc1c0c7da75628550f25a6da81e0d8754
Rou Wen <r3wen@ucsd.edu>
2021-03-03 23:12:07 -0800

    JSON data template for yourRecipes

--- routes/yourRecipes.js
+++ routes/yourRecipes.js
@@ -1,8 +1,11 @@
-
 /*
- * GET create recipe page.
+ * GET your recipe page.
  */
+var saved = require('../data_recipes_saved.json');
+var created = require('../data_recipes_created.json');
 
 exports.view = function(req, res){
-  res.render('yourRecipes');
+  res.render('yourRecipes', {
+  "recipes": [saved, created]
+  });
 };
